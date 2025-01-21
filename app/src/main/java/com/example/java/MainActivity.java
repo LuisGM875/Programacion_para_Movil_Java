0867c765adcabed608dd71bd971a45ddc0b81265
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-21 12:13:05 -0600

    feat: add view for product search

--- app/src/main/java/com/example/java/MainActivity.java
+++ app/src/main/java/com/example/java/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.java;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Spinner spinnerCategoria = findViewById(R.id.spinnerCategoria);
+        TextView textViewCategoria = findViewById(R.id.textViewCategoria);
+
+        String[] categorias = {
+                "Seleccione una opción",
+                "Electrónica",
+                "Ropa",
+                "Hogar"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categorias) {
+            @Override
+            public boolean isEnabled(int position) {
+                return position != 0;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, android.view.ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                if (position == 0) {
+                    view.setEnabled(false);
+                }
+                return view;
+            }
+        };
+        spinnerCategoria.setAdapter(adapter);
+        spinnerCategoria.setSelection(0);
+        spinnerCategoria.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    return;
+                } else {
+                    String selectedCategoria = parent.getItemAtPosition(position).toString();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
     }
 }
