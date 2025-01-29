ab01f21464f48f6e339e460baba30177e058b5e9
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-28 23:02:19 -0600

    feat: add functionality to preferences form

--- app/src/main/java/com/example/java/MainActivity.java
+++ app/src/main/java/com/example/java/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.java;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText colorEditText = findViewById(R.id.editTextText2);
+        EditText foodEditText = findViewById(R.id.editTextText3);
+        Button sendButton = findViewById(R.id.button);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String favoriteColor = colorEditText.getText().toString();
+                String favoriteFood = foodEditText.getText().toString();
+                Toast.makeText(MainActivity.this, "Favorite Color: " + favoriteColor + ", Favorite Food: " + favoriteFood, Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
