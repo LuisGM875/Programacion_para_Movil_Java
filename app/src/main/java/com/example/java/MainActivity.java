2b02ee375c2ad61b321fb5d81b646920ee3c88b5
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-28 22:35:44 -0600

    feat: add functionality to view

--- app/src/main/java/com/example/java/MainActivity.java
+++ app/src/main/java/com/example/java/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.java;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText nameEditText = findViewById(R.id.editTextText2);
+        EditText emailEditText = findViewById(R.id.editTextText3);
+        EditText passwordEditText = findViewById(R.id.editTextTextPassword);
+        Button registerButton = findViewById(R.id.button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                Toast.makeText(MainActivity.this, "Name: " + name + ", Email: " + email + ", Password: " + password, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
